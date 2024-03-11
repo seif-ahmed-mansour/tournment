38274d97800810803ef70ad26211fea741e91d11
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2024-03-11 23:41:04 +0200

    first commit🚀

---
+++ app/Models/Event.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Event extends Model
+{
+    use HasFactory;
+    protected $fillable = ["name", "type", "score"];
+    public function questions(): HasMany
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function participants(): HasMany
+    {
+        return $this->hasMany(EventParticipant::class);
+    }
+}
---
+++ app/Models/Question.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Question extends Model
+{
+    use HasFactory;
+    protected $fillable = ["question", "answer", "event_id"];
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+}
