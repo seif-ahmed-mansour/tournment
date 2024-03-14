832e5fe03860ed86430a749907544027118645b2
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2024-03-14 23:19:30 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     if (Auth::check() && Auth::user()->role === 'admin') {
         return redirect('/admin');
@@ -24,28 +13,36 @@ Route::get('/', function () {
     }
 });
 
-// Login
-Route::get('/login', [AuthController::class, "showLoginForm"])->name('login');
-Route::post('/login', [AuthController::class, "login"]);
-Route::post('/logout', [AuthController::class, "logout"])->name("logout");
-// Registration
-Route::get('/register', [AuthController::class, "showRegistrationForm"])->name('register');
-Route::post('/register', [AuthController::class, "register"]);
-Route::get("/admin", function () {
-    return view("adminDashboard");
-})->middleware('auth', 'admin');
-Route::get("/admin", [EventController::class, "index"])->middleware("auth", "admin");
-Route::get("/admin/addEvent", [EventController::class, "add"])->name("addEvent");
-Route::post('/admin/choose-leader', [EventController::class, 'chooseLeader'])->name('chooseLeader');
-Route::post('/events', [EventController::class, 'store'])->name('storeEvent');
+// Authentication Routes
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/login', [AuthController::class, "showLoginForm"])->name('login');
+    Route::post('/login', [AuthController::class, "login"]);
+    Route::post('/logout', [AuthController::class, "logout"])->name("logout");
+    Route::get('/register', [AuthController::class, "showRegistrationForm"])->name('register');
+    Route::post('/register', [AuthController::class, "register"]);
+});
 
-Route::post('/participate', [EventController::class, 'participate'])->name('participateEvents');
-Route::get('/events/seats-completed', [EventController::class, 'eventSeatsCompleted'])->name('eventSeatsCompleted');
+// Admin Routes
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [EventController::class, "index"]);
+    Route::get('/admin/addEvent', [EventController::class, "add"])->name("addEvent");
+    Route::post('/admin/choose-leader', [EventController::class, 'chooseLeader'])->name('chooseLeader');
+    Route::post('/events', [EventController::class, 'store'])->name('storeEvent');
+});
 
-Route::get('/events/{event}', [EventController::class, 'showQuestions'])->name('showQuestions');
-Route::post('/events/{event}/submit', [EventController::class, 'submitAnswers'])->name('submitAnswers');
-Route::get('/leaderboard', [EventController::class, 'leaderboard'])->name('leaderboard');
-Route::get('/events/{event}/leaderboard', [EventController::class, 'eventLeaderboard'])->name('eventLeaderboard');
-Route::get("/congrats", function () {
-    return view("congrats");
-})->name("congrats");
+// Event Routes
+Route::middleware(['auth'])->group(function () {
+    Route::post('/participate', [EventController::class, 'participate'])->name('participateEvents');
+    Route::get('/events/seats-completed', [EventController::class, 'eventSeatsCompleted'])->name('eventSeatsCompleted');
+    Route::get('/events/{event}', [EventController::class, 'showQuestions'])->name('showQuestions');
+    Route::post('/events/{event}/submit', [EventController::class, 'submitAnswers'])->name('submitAnswers');
+    Route::get('/events/{event}/leaderboard', [EventController::class, 'eventLeaderboard'])->name('eventLeaderboard');
+});
+
+// Leaderboard and Congrats Routes
+Route::middleware(['auth'])->group(function () {
+    Route::get('/leaderboard', [EventController::class, 'leaderboard'])->name('leaderboard');
+    Route::get('/congrats', function () {
+        return view("congrats");
+    })->name("congrats");
+});
