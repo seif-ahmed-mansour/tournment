7ed8826637b0805d92a0046d87661ddc8d116e78
seif-ahmed-mansour <seifahmedahmed111@gmail.com>
2024-03-14 23:09:01 +0200

    .

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.layout')
 @section('body')
     <div class="d-flex justify-content-center align-items-center" style="height: 100vh">
-        <div>
-            <h1>great job,</h1>
-            <a href="{{ route('leaderboard') }}" class="btn btn-info">view leaderboards</a>
+        <div class="text-center">
+            <h1>great job you completed your events,</h1>
+            <a href="{{ route('leaderboard') }}" class="btn btn-info ">view leaderboards</a>
         </div>
     </div>
 @endsection
